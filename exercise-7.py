1dc41d5213db366078eaa715f17120f6f8c3d6b5
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-22 19:31:33 +0800

    Def function

--- exercise-7.py
+++ exercise-7.py
@@ -1,6 +1,8 @@
 # Return the count of a given substring from a string
 
 # Count the substrings in the string
+def string_count(string, substring):
+    return substring.count(substring)
 
 # Given string
 
