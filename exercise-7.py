5efdf2bc996de21186a72c49d0ca7fe88691b2e5
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-22 19:36:29 +0800

    Given string

--- exercise-7.py
+++ exercise-7.py
@@ -1,12 +1,12 @@
 # Return the count of a given substring from a string
 
 # Count the substrings in the string
-def string_count(string, substring):
-    return substring.count(substring)
+def substring_count(string, substring):
+    return string.count(substring)
 
 # Given string
+string = "I have 7 cats. The cat is on the mat. I love cats"
 
 # Count the substrings
 
 # Print the results
-
